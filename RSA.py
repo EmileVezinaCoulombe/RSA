8b900549f7517d484faa6f177057fff32eef81b8
Émile Vézina-Coulombe <EMVEC1@ulaval.ca>
2020-11-24 22:27:51 -0500

    Add fonctions

--- RSA.py
+++ RSA.py
@@ -1,39 +1,5 @@
-import random
 import textwrap
-
-
-table_cryptage = {
-                    'a': '001', 'b': '002', 'c': '003', 'd': '004', 'e': '005', 'f': '006', 'g': '007', 'h': '008',
-                    'i': '009', 'j': '010', 'k': '011', 'l': '012', 'm': '013', 'n': '014', 'o': '015', 'p': '016',
-                    'q': '017', 'r': '018', 's': '019', 't': '020', 'u': '021', 'v': '022', 'w': '023', 'x': '024',
-                    'y': '025', 'z': '026', 'A': '027', 'B': '028', 'C': '029', 'D': '030', 'E': '031', 'F': '032',
-                    'G': '033', 'H': '034', 'I': '035', 'J': '036', 'K': '037', 'L': '038', 'M': '039', 'N': '040',
-                    'O': '041', 'P': '042', 'Q': '043', 'R': '044', 'S': '045', 'T': '046', 'U': '047', 'V': '048',
-                    'W': '049', 'X': '050', 'Y': '051', 'Z': '052', '0': '053', '1': '054', '2': '055', '3': '056',
-                    '4': '057', '5': '058', '6': '059', '7': '060', '8': '061', '9': '062', ' ': '063', "'": '064',
-                    ':': '065', ';': '066', '.': '067', '<': '068', '>': '069', '«': '070', '»': '071', '%': '072',
-                    '=': '073', '-': '074', '+': '075', '/': '076', '\\': '077', '*': '078', '#': '079', '@': '080',
-                    '!': '081', '?': '082', '&': '083', '(': '084', ')': '085', '_': '086', '{': '087', '}': '088',
-                    '"': '089', ',': '090', '$': '091', '[': '092', ']': '093', 'é': '094', 'è': '095', 'ê': '096',
-                    'É': '097', 'È': '098', 'Ê': '099', 'à': '100', 'À': '101', 'ç': '102', 'Ç': '103', 'ù': '104',
-                    'Ù': '105', 'î': '106', 'Î': '107'
-                }
-
-
-def inverse_dico(dico):
-    """
-    Fonction inverse la clé avec sa valeur dans un dictionnaire.
-        ex: {'A':1} => {'1':'A'}.
-    Prend en agrument un dictionnaire.
-    Retourne un dictionnaire.
-    """
-    inv_dico = {}
-    for keys in dico:
-        inv_dico[dico[keys]] = keys
-    return inv_dico
-
-
-table_decryptage = inverse_dico(table_cryptage)
+from random import randint as rand
 
 
 def num_condition(number):
@@ -122,30 +88,23 @@ def gcd(number1, number2):
         reste2%reste3 = 35%21 = 14 = reste4
         reste3%reste4 = 21%14 = 7 = reste5
         reste4%reste5 = 14%7 = 0 = reste6
-        gcd(91, 126) = reste5
+        gcd(91, 126) = reste5 = 7
     Prend en agrument un nombre.
         1-Associe un nombre arbitraire sans importance, mais > 0.
         2-Boucle tant que remainder != 0.
         3-remaider = num1*mod(num2).
         4-num2 devient num1 et remainder devient num2.
     Retourne un num.
+
+    Si gcd(i, j) = 1, alors i et j sont co-premier
     """
-    remainder = 1   #1-
-    while remainder > 0:    #2-
-        remainder = number1 % number2   #3-
-        number1, number2 = number2, remainder
-    return number1
-
-
-def base107(num):
-    if num > 107:
-        num = num % 107
+    if number2 == 0:
+        return number1
     else:
-        return num
-    return base107(num)
+        return gcd(number2, number1%number2)
 
 
-def period(arg, n):
+def period(arg, n, exponent = 1,count = 1):
     """
     Fonction détermine la période pour que (arg**k)mod(n) = 1.
         ex: (2**k)mod(7)
@@ -156,34 +115,149 @@ def period(arg, n):
     Prend en agrument deux nombres.
     Retourne un numbre.
     """
-    remainder = 0
-    laps = 0
-    exponent = 1
-    while remainder != 1:
-        remainder = (arg ** exponent) % n
-        exponent += 1
-        laps += 1
-    return laps
+    if (arg**exponent)%n == 1:
+        return count
+    exponent += 1
+    count += 1
+    return period(arg, n, exponent, count)
 
 
-def three_digit(number):
+def mod_inverse(e, phi):
     """
-    Fonction rend un nombre sur trois digits.
-    Prend en agrument un nombre.
-    Retourne un string.
+    Fonction détermine l'inverse modulaire.
+    Prend en agrument e, phi.
+        trouve le multiple d pour que (e*d)%phi =1
+    Retourne d.
     """
-    if number < 100:
-        digit = '0' + str(number)
-        if number < 10:
-            digit = '0' + digit
-    else:
-        digit = str(number)
-    return digit
+    for d in range(1, phi):
+        if (e*d) % phi == 1:
+            return d
+    return -1
+
+
+def gen_phi(p, q):
+    """
+    Génère phi_n = (p - 1)*(q - 1)
+    phi est le nombre de facteurs co-premier avec N
+    """
+    return (p - 1)*(q - 1)
+
+
+def coprime(e, phi):
+    if gcd(e, phi_n) == 1:
+        return True
+    return False
+
+
+def gen_pseudo_e(phi, e = 2, bit_lenght = 1):
+    """
+    Fonction génaire un e temporaire.
+    Prend en agrument e, phi et une longeure min en nombre de bit pour e.
+    La convertion bit_lenght(b) et digit(n) : b = log_2(n + 1)
+                                                n = 2**b - 1
+    Retourne e.
+    """
+    if e < (2**bit_lenght - 1):
+        e = 2**bit_lenght
+    if e > phi:
+        return -1
+    if gcd(e, phi) == 1 and e < phi:
+        return e
+
+    return gen_pseudo_e(phi, e+1, bit_lenght)
+
+
+def gen_e_d(phi, bit_lenght = 1, e = 2):
+    """
+    Fonction génaire un e et d.
+    Prend en agrument e, phi et une longeure min en nombre de bit pour e.
+        génaire un d puis un e et regarde les conditions
+        si incapable de générer e et d lève une erreur 
+    Retourne tuple (e, d).
+    """
+    d = mod_inverse(e, phi)
+    e = gen_pseudo_e(phi, bit_lenght, e)
+    if gcd(e, phi) == 1 and e != d and d != -1 and e != -1:
+        return e, d
+    if d == -1 and e == -1:
+        raise Exception("La création de e et d est imposible, choisisez d'autres p et q")
+    return gen_e_d(phi, bit_lenght, e+1)
+
+
+def gen_keys(p, q, bit_lenght = 1):
+    """
+    Fonction génaire les clés.
+    Prend en agrument p, q et un longeur min en nb de bit.
+        trouve n
+        trouve phi
+        trouve e et d
+    Retourne tuple ((e, n), (d, n)).
+    """
+    n = p*q
+    phi = gen_phi(p, q)
+    try:
+        e, d = gen_e_d(phi, bit_lenght)
+    except:
+        print("La création de e et d est imposible, choisisez d'autres p et q")
+    return ((e, n), (d, n))
+
+
+def encrypt(message, public_key):
+    """
+    Fonction encrypt le message.
+    Prend en agrument le message (str) et la clé publique.
+        ord retourne la valeur numérique(unicode) d'un str 
+            ex : ord(a) = 97
+        pow(a, b, c) retourne (a**b)%c
+    Retourne une liste de la valeur num de chaque lettre du message.
+        ex: ['32', '103', '98']
+    """
+    e, n = public_key
+    retourne [pow(ord(lettre), e, n) for lettre in message]
+
+
+def decrypt(msg_crypt, private_key):
+    """
+    Fonction décrypt le message crypter.
+    Prend en agrument le message crypter (list, voir fonc encrypt) et la clé privé.
+        chr retourne le str d'une valeur numérique(unicode)  
+            ex : char(97) = a
+        pow(a, b, c) retourne (a**b)%c
+    Retourne une liste de lettre du message décrypter.
+        ex: ['H', 'e', 'l', 'l', 'o']
+    """
+    d, n = private_key
+    return [char(pow(crypt, d, n) for crypt in msg_crypt)]
+
+
+def show_encrypt(liste):
+    """
+    Fonction regroupe le message crypté.
+    Prend en agrument le message crypter (list, voir fonc encrypt).
+        chr retourne le str d'une valeur numérique(unicode)  
+            ex : char(97) = a
+    Retourne le message crypter.
+        ex: 'Hello'
+    """
+    message = ''
+    for number in liste:
+        message += char(liste[number])
+    return message
+
+
+def show_decrypt(liste):
+    """
+    Fonction regroupe le message décrypté.
+    Prend en agrument le message décrypter (list, voir fonc encrypt).
+    Retourne le message décrypter.
+        ex: 'Hello'
+    """
+    return (''.join(liste))
 
 
 def justify(text, width=70, sep=' ', justify_last_line=False):
     """
-    Fonction de la librairie justify
+    Fonction de la librairie Textwrap
     Justifie un texte pour l'imprimer dans le terminal
     retourne une liste
     """
@@ -252,6 +326,8 @@ def print_justify(text, width=70, close=False, sep=' ', justify_last_line=False)
         print(line)
 
 
+
+
 def alice_create_clef(len_text=70):
     context = "Bob veut envoyer un message d\'amour à Alice sans que personne " \
                 "ne puisse intercepter leurs ferveurs. Alice doit donc créer " \
@@ -265,32 +341,15 @@ def alice_create_clef(len_text=70):
 
     print_justify(context, len_text)
     print_justify(prime_context, len_text)
-    number_1 = input("Premier nombre : ")
+    text_number_1 = input("Premier nombre : ")
     prime_1 = find_prime(number_1)
-    number_2 = input("Deuxième nombre : ")
+    text_number_2 = input("Deuxième nombre : ")
     prime_2 = find_prime(number_2)
     print_justify(f"Le couple de nombre premier (p, q) est : {prime_1}, {prime_2}", len_text, True)
-    N = prime_1*prime_2
+    
     print_justify(f"Produit des deux nombres premiers nommés N = {N}", len_text, True)
-    phi_n = ((prime_1 - 1)*(prime_2 - 1))
     print_justify(f"Phi_n = (prime1-1)(prime2-1) = {phi_n}", len_text, True)
-    e_choices = []
-    i = 3
-    while i < phi_n:
-        if gcd(i, phi_n) == 1:
-            if gcd(i, N) == 1:
-                e_choices.append(i)
-        i += 1
-        if i > N/2:
-            i = phi_n
-    e = e_choices[-1]
 
-    # d*e(mod(phi_n))=1 => d=k*(phi_n)-1
-    d_choises = []
-    for k in range(1,3):
-        d_choises.append(int((k * phi_n) - 1))
-
-    d = d_choises[-1]
     print_justify(f"Choix de d => d*e % phi_n = 1 : {d_choises}", len_text, True)
     print_justify(f"Choix de e => copremier avec N et phi_n dans ]1, phi_n[ : {e_choices}", len_text)
     print_justify(f"La clé publique (e, N) = ({e}, {N})", len_text, True)
@@ -347,3 +406,5 @@ def alice_decrypt(len_text=70):
         message_decrypt += table_decryptage[three_digit(mod_decrypt)]
 
     print_justify(f'Le message reçu : {message_decrypt}', len_text)
+
+
